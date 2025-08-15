23513701b1a01285cfa96a3285fb97fee11c25dc
Peter Barraud <gapeterb@gmail.com>
2025-08-15 12:47:55 +0530

    use the TopicTitle prop to not load the CorrectAnswers class all the time

--- add-q-and-a/libs/correctanswers.py
+++ add-q-and-a/libs/correctanswers.py
@@ -2,9 +2,10 @@ from re import match as rematch
 from os.path import isfile
 
 class CorrectAnswers:
-    def __init__(self, ans_file_location) -> None:
+    def __init__(self, ans_file_location, topic_title) -> None:
         self.__correctanswers : dict = dict()
         self.__correctanswers_file_found = True
+        self.__topic_title = topic_title
         if isfile(f'{ans_file_location}/correct.choices'):
             with open(f'{ans_file_location}/correct.choices', encoding='utf-8') as f:
                 for l in [x.strip() for x in f if x.strip() != '']:
@@ -17,6 +18,10 @@ class CorrectAnswers:
     @property
     def CorrectAnswerFileFound(self):
         return self.__correctanswers_file_found
+    
+    @property
+    def TopicTitle(self):
+        return self.__topic_title
 
     def getCorrectOptionNames(self, questionNumber):
         if self.__correctanswers.get(questionNumber, None):
