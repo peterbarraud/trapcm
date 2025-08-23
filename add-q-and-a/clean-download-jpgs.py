2f02b52c62912e59704b0a4ce472d56ffe7a84f6
Peter Barraud <gapeterb@gmail.com>
2025-08-23 21:35:33 +0530

    just cleaned another dir

--- add-q-and-a/clean-download-jpgs.py
+++ add-q-and-a/clean-download-jpgs.py
@@ -6,7 +6,7 @@ def main(files_dir):
 
     for full_file_path in glob(f"{files_dir}/*jpg"):
         file_name = os.path.basename(full_file_path)
-        short_file_name = file_name.replace('Physics - JEE Main 2025 January Chapter-wise Question Bank - MathonGo_page-','')
+        short_file_name = file_name.replace('Chemistry - JEE Main 2025 January_page-','')
         print(short_file_name)
         print(full_file_path)
         os.rename(full_file_path,f"{files_dir}/{short_file_name}")
@@ -16,4 +16,4 @@ def main(files_dir):
     f_log.close()
 
 if __name__ == '__main__':
-    main(r'D:\tech-stuff\trapcm\qanda-files\jee\2025-jan\physics\ilovepdf_pages-to-jpg')
+    main(r'D:\tech-stuff\trapcm\qanda-files\raw-data\chemistry\2025\jan-mathongo\ilovepdf_pages-to-jpg')
